41de4be1f38c5a9e882edde8aaf39ec04b3e60ba
Aweryc <93672316+Aweryc@users.noreply.github.com>
2022-02-14 19:34:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,9 @@
 # ТОКЕН ДЛЯ ТЕСТА - @mybot_92_bot
-# tg_bot_token = "2067986686:AAFvGQwAcczT9N_5iBbkGDOgRbb8ECuMJoQ"
+# tg_bot_token = "*****"
 
 # ТОКЕН ДЛЯ ТЕСТА - @aweryc_test_bot
-tg_bot_token = "2114157134:AAGgu_65CZ36_KhA3Ec991ffqRHOq_T4rbM"
+tg_bot_token = "*****"
 
 
-# ОСНОВНОЙ ТОКЕН
-# tg_bot_token = "2067986686:AAFvGQwAcczT9N_5iBbkGDOgRbb8ECuMJoQ"
-
-# Часть ссылки для доступа к гугл таблице
-# google_sheet_link = '1ejIzo82_NNuocfHiCAnA6wYxhqOzk5IQaqh7Qn-cV24'
 
 
-# Адрес к гугл таблице моей
-'https://docs.google.com/spreadsheets/d/1ejIzo82_NNuocfHiCAnA6wYxhqOzk5IQaqh7Qn-cV24/edit#gid=0'
-
-
-# Адрес к гугл от Влада
-'https://docs.google.com/spreadsheets/d/1-b9C6frvg8EUXnjchO9ysUOOAAtbLNkxypQr1os0lgw/edit'
-
-
-# Часть ссылки для доступа к гугл таблице от Влада
-google_sheet_link = '1-b9C6frvg8EUXnjchO9ysUOOAAtbLNkxypQr1os0lgw'
